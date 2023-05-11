5b4fbfd9a604fc2800b3677bd446513c6542372c
Tomasz Grzywacz <tomaszgrzywacz92@gmail.com>
2023-05-11 09:12:40 +0200

    Add project files.

---
+++ Word/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Word
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.btnBold = new System.Windows.Forms.Button();
+            this.btnItalic = new System.Windows.Forms.Button();
+            this.btnUnderline = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbFont = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbFontSize = new System.Windows.Forms.ComboBox();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.pbColor = new System.Windows.Forms.PictureBox();
+            this.rtbContent = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbColor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(12, 12);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 0;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(93, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Location = new System.Drawing.Point(174, 12);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveAs.TabIndex = 2;
+            this.btnSaveAs.Text = "SaveAs";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            // 
+            // btnBold
+            // 
+            this.btnBold.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnBold.Location = new System.Drawing.Point(12, 41);
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(75, 23);
+            this.btnBold.TabIndex = 3;
+            this.btnBold.Text = "B";
+            this.btnBold.UseVisualStyleBackColor = true;
+            // 
+            // btnItalic
+            // 
+            this.btnItalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnItalic.Location = new System.Drawing.Point(93, 41);
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(75, 23);
+            this.btnItalic.TabIndex = 4;
+            this.btnItalic.Text = "I";
+            this.btnItalic.UseVisualStyleBackColor = true;
+            // 
+            // btnUnderline
+            // 
+            this.btnUnderline.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnUnderline.Location = new System.Drawing.Point(174, 41);
+            this.btnUnderline.Name = "btnUnderline";
+            this.btnUnderline.Size = new System.Drawing.Size(75, 23);
+            this.btnUnderline.TabIndex = 5;
+            this.btnUnderline.Text = "U";
+            this.btnUnderline.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Font";
+            // 
+            // cmbFont
+            // 
+            this.cmbFont.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFont.FormattingEnabled = true;
+            this.cmbFont.Location = new System.Drawing.Point(289, 41);
+            this.cmbFont.Name = "cmbFont";
+            this.cmbFont.Size = new System.Drawing.Size(121, 21);
+            this.cmbFont.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(416, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Font size";
+            // 
+            // cmbFontSize
+            // 
+            this.cmbFontSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFontSize.FormattingEnabled = true;
+            this.cmbFontSize.Location = new System.Drawing.Point(471, 41);
+            this.cmbFontSize.Name = "cmbFontSize";
+            this.cmbFontSize.Size = new System.Drawing.Size(121, 21);
+            this.cmbFontSize.TabIndex = 9;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(598, 41);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 10;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            // 
+            // pbColor
+            // 
+            this.pbColor.Location = new System.Drawing.Point(679, 41);
+            this.pbColor.Name = "pbColor";
+            this.pbColor.Size = new System.Drawing.Size(23, 23);
+            this.pbColor.TabIndex = 11;
+            this.pbColor.TabStop = false;
+            // 
+            // rtbContent
+            // 
+            this.rtbContent.Location = new System.Drawing.Point(12, 70);
+            this.rtbContent.Name = "rtbContent";
+            this.rtbContent.Size = new System.Drawing.Size(776, 518);
+            this.rtbContent.TabIndex = 12;
+            this.rtbContent.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.rtbContent);
+            this.Controls.Add(this.pbColor);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.cmbFontSize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbFont);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUnderline);
+            this.Controls.Add(this.btnItalic);
+            this.Controls.Add(this.btnBold);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnNew);
+            this.Name = "Form1";
+            this.Text = "Mój Word";
+            ((System.ComponentModel.ISupportInitialize)(this.pbColor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Button btnBold;
+        private System.Windows.Forms.Button btnItalic;
+        private System.Windows.Forms.Button btnUnderline;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbFont;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbFontSize;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.PictureBox pbColor;
+        private System.Windows.Forms.RichTextBox rtbContent;
+    }
+}
+
